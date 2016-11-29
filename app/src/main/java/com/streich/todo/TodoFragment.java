a6ecc946c6d92d89e286f04395b987fad66b5207
Sebastian Streich <streich.mobile@gmail.com>
2016-11-29 18:30:21 +0100

    Added an Floating Action Button

--- app/src/main/java/com/streich/todo/TodoFragment.java
+++ app/src/main/java/com/streich/todo/TodoFragment.java
@@ -1,12 +1,16 @@
 package com.streich.todo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.internal.NavigationMenu;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -18,11 +22,16 @@ import com.example.antonimuller.fhflapp.R;
 
 import java.util.ArrayList;
 
+import io.github.yavski.fabspeeddial.FabSpeedDial;
+import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 /**
  * Created by Sebastian Streich and Antoni Müller on 25.10.2016.
  */
 
-public class TodoFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class TodoFragment extends Fragment implements  AdapterView.OnItemClickListener, FabSpeedDial.MenuListener {
     final static String key ="Todo-Fragment";
 
 
@@ -41,6 +50,10 @@ public class TodoFragment extends Fragment implements View.OnClickListener, Adap
         todoList = (ListView) fragmentView.findViewById(R.id.listV);
         title_input = (TextView) fragmentView.findViewById(R.id.todo_title_input);
 
+        //Action Button initialisieren.
+        FabSpeedDial fabSpeedDial = (FabSpeedDial) fragmentView.findViewById(R.id.todo_appButton);
+        fabSpeedDial.setMenuListener(this);
+
         //Fetch the Model
         model = new TodoListHolder();
         //Loading stuff out of the LocalStorage.
@@ -52,41 +65,59 @@ public class TodoFragment extends Fragment implements View.OnClickListener, Adap
         todoList.setAdapter(x);
 
         //Set EventListners
-        fragmentView.findViewById(R.id.AddButton).setOnClickListener(this);
         todoList.setOnItemClickListener(this);
 
 
         return fragmentView;
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.AddButton:
-                //Add a New item to the List.
-                String input = title_input.getText().toString();
-
-                if(input.length()>0){
-                    Log.v(key,"Adding new Todo");
-                    model.add(new TodoModel(title_input.getText().toString()));
-
-                    title_input.setText("");
-                    x.notifyDataSetChanged();
-                }
-                break;
-        }
-    }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         //Some Click on an item.
         Log.v(key,"Clicked on an Item");
-
         TodoModel clickedItem = x.getItem(position);
         clickedItem.done= !clickedItem.done;
-
         x.notifyDataSetChanged();
     }
+
+    @Override
+    public boolean onPrepareMenu(NavigationMenu navigationMenu) {
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(MenuItem menuItem) {
+        //TODO: Start some activity
+        switch (menuItem.getItemId()){
+            case R.id.todo_new_category:
+                Log.v(key,"New Category");
+
+                break;
+            case R.id.todo_new_todo:
+                Log.v(key,"New Todo");
+                //Prepare a Add an TODO Dialog.
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setPositiveButton("Speichern", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                    }
+                });
+                builder.setNegativeButton("Abbruch", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                         }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+        }
+        return false;
+    }
+
+    public void onMenuClosed(){}
+
 
     @Override
     public void onPause(){
@@ -105,4 +136,18 @@ public class TodoFragment extends Fragment implements View.OnClickListener, Adap
 
 Context con = getActivity();
                 model.commmit(con);
+
+
+
+
+                //Add a New item to the List.
+                String input = title_input.getText().toString();
+
+                if(input.length()>0){
+                    Log.v(key,"Adding new Todo");
+                    model.add(new TodoModel(title_input.getText().toString()));
+
+                    title_input.setText("");
+                    x.notifyDataSetChanged();
+                }
  */
